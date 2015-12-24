2c1c24953c8de08860d0ae5d7e294d8eca5699d1
Keita Furui <kta@live.jp>
2015-12-24 23:37:48 +0900

    Hash: テーブルサイズを最小限に

--- src/hash.d
+++ src/hash.d
@@ -4,15 +4,15 @@ struct Hash {
   ulong _key;
   alias _key this;
 
-  void update(uint to, uint koma) @nogc { _key ^= _zobrist[koma][to]; }
+  void update(const uint to, const uint koma) @nogc { _key ^= _zobrist[koma - 4][to]; }
 
-  static immutable ulong[82][32] _zobrist = initZobrist();
-  static ulong[82][32] initZobrist() {
-    ulong[82][32] z;
+  static immutable ulong[81][28] _zobrist = initZobrist();
+  static ulong[81][28] initZobrist() {
+    ulong[81][28] z;
     Random gen;
     gen.seed(77);
     foreach (koma_i; 4..32)
-      foreach (square_i; 0..81) { z[koma_i][square_i] = uniform(ulong.min, ulong.max, gen) << 1; }
+      foreach (square_i; 0..81) { z[koma_i - 4][square_i] = uniform(ulong.min, ulong.max, gen) << 1; }
     return z;
   }
 }
