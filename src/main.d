a2343d9e6d2711b8cfd2eb16cc670004a9df460c
Keita Furui <kta@live.jp>
2016-08-08 23:48:51 +0900

    main: STR_INIT

--- src/main.d
+++ src/main.d
@@ -1,25 +1,19 @@
-import std.random;
-import std.datetime;
-import std.stdio;
-import std.conv;
-import std.algorithm;
-import std.array;
-import std.regex;
-import std.string;
-
-import shogiban;
-import move;
-import testcode;
+import std.stdio, std.algorithm, std.array, std.conv, std.range, std.string;
+import shogiban, testcode, move;
 
 void main() {
-  auto s = new Shogiban;
-  s.writeln;
+  mixin(STR_INIT);
 
   //コマンド受付
   foreach (args; stdin.byLine.map !(a => a.chain(" (null)").to !string.strip.split))
     switch (args[0])
-      mixin(strCommands);  //コマンドの展開
+      mixin(STR_COMMAND);  //コマンドの展開
 }
+
+immutable string strInitMain = q{
+  auto s = new Shogiban;
+  s.writeln;
+};
 
 immutable string strCaseMain = q{
   case "init", "i":  //盤面の初期化
@@ -33,10 +27,12 @@ immutable string strCaseMain = q{
     return;
   case "help":  //ヘルプ(caseの表示)
   default:
-    strCommandList.writeln;
+    STR_COMMANDLIST.writeln;
     break;
 };
 
 //コンパイル時にファイル内を検索してコマンドっぽい文字列変数を収集して結合する
-immutable string strCommands = mixin(import(__FILE__).strip.split.filter !(a => a.startsWith("strCase")).join("~"));
-immutable string strCommandList = strCommands.split("\n").map !strip.filter !(a => a.startsWith("case")).map !(a => a[5.. $]).join("\n");
+immutable string STR_COMMAND = mixin(import(__FILE__).strip.split.filter !(a => a.startsWith("strCase")).join("~"));
+immutable string STR_COMMANDLIST = STR_COMMAND.split("\n").map !strip.filter !(a => a.startsWith("case")).map !(a => a[5.. $]).join("\n");
+// mainの最初に実行するコード断片
+immutable string STR_INIT = mixin(import(__FILE__).strip.split.filter !(a => a.startsWith("strInit")).join("~"));
