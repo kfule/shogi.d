587f8593f18aef9921a3bf1f1791eeaf933e5280
Keita Furui <kta@live.jp>
2016-11-03 15:30:39 +0900

    main: refactoring

--- src/main.d
+++ src/main.d
@@ -1,33 +1,19 @@
-import std.random;
-import std.datetime;
-import std.stdio;
-import std.conv;
-import std.algorithm;
-import std.array;
-import std.regex;
-import std.string;
-
-import shogiban;
-import move;
-import testcode;
+import std.stdio, std.algorithm, std.array, std.conv, std.range, std.string;
+import shogiban, testcode, move;
 
 void main() {
-  auto s = new Shogiban;
-  s.writeln;
+  mixin(STR_INIT);
 
   //コマンド受付
-  string line;
-  while ({
-    write(">");
-    return ((line = stdin.readln()) !is null);
-  }()) {
-    if (line.strip() == "") line = "(null)";
-    string[] args = line.split();
-    switch (args[0]) {
-      mixin(strCommands);  //コマンドの展開
-    }
-  }
+  foreach (args; stdin.byLine.map !(a => a.chain(" (null)").to !string.strip.split))
+    switch (args[0])
+      mixin(STR_COMMAND);  //コマンドの展開
 }
+
+immutable string strInitMain = q{
+  auto s = new Shogiban;
+  s.writeln;
+};
 
 immutable string strCaseMain = q{
   case "init", "i":  //盤面の初期化
@@ -41,34 +27,12 @@ immutable string strCaseMain = q{
     return;
   case "help":  //ヘルプ(caseの表示)
   default:
-    printCaseText(args[0]);
+    STR_COMMANDLIST.writeln;
     break;
 };
 
-//実行できるコマンドを画面に表示する
-void printCaseText(string arg) {
-  if (arg != "help") writeln("invalid command: " ~arg);
-  writeln("\nコマンドは下記の通り");
-  auto r = regex(r"(?<=case\s)\u0022.+[^\n]");
-  foreach (c; matchAll(strCommands, r)) {
-    writef("%-16s", replaceAll(c.hit, regex(r"\u0022|//.*|:"), ""));
-    writeln(replaceAll(c.hit, regex(r".*\u0022|//[\p{WhiteSpace}]*|:"), ""));
-  }
-  writeln();
-}
-
 //コンパイル時にファイル内を検索してコマンドっぽい文字列変数を収集して結合する
-immutable string strCommands = mixin({
-  string s = "";
-  auto str = import(__FILE__);
-  uint cnt;
-  while (!str.empty) {
-    str = str.find("str" ~"Case");  //こいつ自体が検索に引っかからないように
-    if (!str.empty) {
-      if (cnt++) s ~= "~";
-      s ~= " " ~str[0..11];  //文字列名は後ろ4文字を含めて11文字
-      str = str[11.. $];
-    }
-  }
-  return s;
-}());
+immutable string STR_COMMAND = mixin(import(__FILE__).strip.split.filter !(a => a.startsWith("strCase")).join("~"));
+immutable string STR_COMMANDLIST = STR_COMMAND.split("\n").map !strip.filter !(a => a.startsWith("case")).map !(a => a[5.. $]).join("\n");
+// mainの最初に実行するコード断片
+immutable string STR_INIT = mixin(import(__FILE__).strip.split.filter !(a => a.startsWith("strInit")).join("~"));
